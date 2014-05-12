fb86eb821854e6b9d2767653ce1e6e9cfe6e48b6
nrs365 <nrs365@gmail.com>
2014-05-12 11:34:34 -0500

    added number of guesses incremeter and removed echo fo guesses from inside the loop

--- high_low.php
+++ high_low.php
@@ -7,25 +7,25 @@
 // - if a user guesses the number, the game should declare "GOOD GUESS!"
 
 $number = mt_rand(1,100);
+$number_of_guesses = 1;
 
+fwrite(STDOUT, "Guess a number!\n");
 
+	
 
 do {
-	fwrite(STDOUT, "Guess a number!\n");
-
 	$guess = fgets(STDIN);
-
 	if ($number > $guess) {
 		echo "Higher!\n";
-		//fwrite(STDOUT, "Guess again: ");
+		$number_of_guesses++;
+		fwrite(STDOUT, "Guess again: ");
 	} else if ($number < $guess) {
 		echo "Lower!\n";
-		//fwrite(STDOUT, "Guess again ");
-	} else if ($number == $guess) {
-		echo "Good Guess!  My number was {$number}!\n";
+		$number_of_guesses++;
+		fwrite(STDOUT, "Guess again ");
 	}
 } while ($number != $guess);
-	
+	echo "Good Guess!  My number was {$number}! It took {$number_of_guesses} guesses for you to guess my number.\n";
 
 
 exit(0);
